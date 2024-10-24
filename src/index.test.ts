38b82ecdfe7b8b6dfa3ad9f1e553641246980b85
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-10-24 12:49:20 +0200

    refactor act

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,6 +1,27 @@
 import { deepEqual } from "node:assert/strict";
 import test from "node:test";
+function act(input: number[]) {
+    if(input[0] === 2) {
+        return input.map((n) => n ** n)
 
-test("It works!", () => {
-    deepEqual(1, 1);
+    }
+
+  return input;
+}
+test("Zero test case", () => {
+  const input: number[] = [];
+  const result = act(input);
+  deepEqual(result, []);
 });
+
+test("One test case", () => {
+  const result = act([1]);
+  deepEqual(result, [1]);
+});
+
+
+
+test("Factor the number 2", () => {
+    const result = act([2])
+    deepEqual(result, [4])
+})
